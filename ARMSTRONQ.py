a7496e4988c043cdf09f4057bcc8af69ebf8d9d6
murad008 <119792590+murad008@users.noreply.github.com>
2024-05-04 23:14:22 +0400

    private

--- ARMSTRONQ.py
+++ ARMSTRONQ.py
@@ -1,14 +1,1 @@
-n = int(input(": "))
-a=str(n)
-c=[]
-for i in range(len(a)):
-    b= int(a[i])**3
-    c.append(b)
-    toplam = sum(c)
-print(toplam)
 
-if toplam == n:
-    print("Bu armstronq reqemdir")
-else:
-    print("Bu armstronq reqem deyil")
-    
